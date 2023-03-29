498ba17de193e05be123d57f885026e5d8bfe730
jocunda <silviawanshan88@gmail.com>
2023-03-29 20:44:13 +0800

    webpack project setting

---
+++ src/components/App.tsx
@@ -0,0 +1,16 @@
+import "../styles/index.scss"
+import React from "react"
+
+const App = () => {
+    return <>
+        <section className="hero"></section>
+        <main>
+            <section>
+                <h1>Hi, React</h1>
+            </section>
+        </main>
+
+    </>
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+let mode = "development";
+let target = "web";
+
+if (process.env.NODE_ENV === "production") {
+    mode = "production";
+    target = "browserslist";
+}
+
+module.exports = {
+    mode: mode,
+    entry: './src/index.tsx',
+    target: target,
+    module: {
+        rules: [
+            {
+                test: /\.(png|svg|jpe?g|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(s[ac]|c)ss$/i, //support sass,scss,css
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: { publicPath: "" }
+                    },
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: "ts-loader",
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            "@babel/preset-env",
+                            "@babel/preset-react",
+                            "@babel/preset-typescript"
+                        ],
+                    }
+                }
+            }
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new CleanWebpackPlugin()
+    ],
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        assetModuleFilename: "images/[hash][ext][query]",
+    },
+    devtool: "source-map",
+    devServer: {
+        static: './dist',
+        hot: true,
+    }
+}
