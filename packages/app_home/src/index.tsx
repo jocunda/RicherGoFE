777beaf3dd2551372611ef1d10cddbb2531b7c99
jocunda <silviawanshan88@gmail.com>
2023-04-04 20:48:20 +0800

    add app_home package

---
+++ packages/app_home/src/index.tsx
@@ -0,0 +1,13 @@
+import App from "./components/App"
+import React from "react";
+
+import { createRoot } from 'react-dom/client';
+
+const domNode = document.getElementById('root') as HTMLElement;
+const root = createRoot(domNode);
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
